7d2060ba243c477157a3807befef1544885be301
KamoEllen <108501662+KamoEllen@users.noreply.github.com>
2023-04-28 21:24:13 +0200

    Update 9.Simple Bioinformatics DNA Count App.py

--- 9.Simple Bioinformatics DNA Count App.py
+++ 9.Simple Bioinformatics DNA Count App.py
@@ -48,8 +48,31 @@ X = DNA_nucleotide_count(sequence)
 X_label = list(X)
 X_values = list(X.values())
 
- 
-           
+
+st.write(X)
+
+st.subheader('2. Print text')
+st.write('There are ' + str(X['A']) + ' adenine (A)')
+st.write('There are ' + str(X['T']) + ' thymine (T)')
+st.write('There are ' + str(X['G']) + ' guanine (G)')
+st.write('There are ' + str(X['C']) + ' cytosine (C)')
+
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index':'nucleotide'})
+st.write(df)
+
+st.subheader('4. Display Bar Chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+p = p.properties(
+    width=alt.Step(80) # controls width of bar.
+)
+st.write(p)
            
            
            
